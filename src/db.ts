006c1ce8e7e99a675364ef395d8147d064d65106
Alya Kovalenko <kovalenkoalinam@gmail.com>
2024-01-31 17:53:56 +0300

    fix: eslint errors

--- src/db.ts
+++ src/db.ts
@@ -5,7 +5,7 @@ const pool = new pg.Pool({
   user: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'root',
   host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
+  port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
   database: process.env.DB_DATABASE || 'postgres',
 });
 
