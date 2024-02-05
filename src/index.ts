fe646b321a05d44af7d6b91f7ced1b70003983d1
Alya Kovalenko <kovalenkoalinam@gmail.com>
2024-02-05 15:36:52 +0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express, { Application } from 'express';
 import 'dotenv/config';
 import router from './routes/user.routes';
 import wishlistRouter from './routes/wishlist.routes';
+import cors from 'cors';
 
 const port = process.env.PORT || 8080;
 
 const app: Application = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 app.use('/api', wishlistRouter);
