5b536aaf4bf768582be0d70ce1db946007d64bbf
Alya Kavalenka <kovalenkoalinam@gmail.com>
2024-04-04 20:04:34 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Wishlist')
+    .setDescription('Api for wishlist app')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const configService = app.get(ConfigService);
   const port = +configService.get('PORT') || 8080;
